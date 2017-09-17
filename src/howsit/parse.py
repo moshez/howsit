f71ce996865be92b25eda34fa3283b2de0eb2e59
Moshe Zadka <zadka.moshe@gmail.com>
2017-09-16 20:27:39 -0700

    froop

--- src/howsit/parse.py
+++ src/howsit/parse.py
@@ -7,14 +7,16 @@ class _Status(enum.IntEnum):
     UNTRACKED = 0
     UNCOMMITTED = 1
     UNPUSHED = 2
-    NOTGIT = 3
-    OK = 4
+    NO_UPSTREAM = 3
+    NOT_GIT = 4
+    OK = 5
 
 _indicator = {
     _Status.UNTRACKED: '!',
     _Status.UNCOMMITTED: 'C',
     _Status.UNPUSHED: 'P',
-    _Status.NOTGIT: 'G',
+    _Status.NO_UPSTREAM: 'U',
+    _Status.NOT_GIT: 'G',
     _Status.OK: 'K',
 }
 
@@ -25,6 +27,8 @@ def get_problems(output):
     lines = output.splitlines()
     for line in lines:
         if line.startswith(b'#'):
+            if b'...' not in line:
+                yield _Status.NO_UPSTREAM
             if b'[' not in line:
                 continue
             line = line.split(b'[')[1]
@@ -40,7 +44,7 @@ def get_indicator(executor):
     try:
         out, _ = executor.git.status(branch=None, porcelain=None).batch()
     except seashore.ProcessError:
-        problem = _Status.NOTGIT
+        problem = _Status.NOT_GIT
     else:
         problem = min(get_problems(out))
     return _get_indicator(problem)

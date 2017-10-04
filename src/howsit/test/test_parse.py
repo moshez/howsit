1e7860abd3281688d5f227fabd0d5aec43d82347
Moshe Zadka <zadka.moshe@gmail.com>
2017-10-03 18:05:58 -0700

    froop

--- src/howsit/test/test_parse.py
+++ src/howsit/test/test_parse.py
@@ -10,3 +10,41 @@ class ProblemTest(unittest.TestCase):
         ok = l.pop(0)
         self.assertEquals(l, [])
         self.assertEquals(ok.name, 'OK')
+
+    def test_no_upstream(self):
+        output = "## v1\n"
+        l = list(parse.get_problems(output))
+        ok = l.pop()
+        problem = l.pop(0)
+        self.assertEquals(l, [])
+        self.assertEquals(ok.name, 'OK')
+        self.assertEquals(problem.name, 'NO_UPSTREAM')
+
+    def test_no_push(self):
+        output = "## v1...origin/v1 [ahead 1]\n"
+        l = list(parse.get_problems(output))
+        ok = l.pop()
+        problem = l.pop(0)
+        self.assertEquals(l, [])
+        self.assertEquals(ok.name, 'OK')
+        self.assertEquals(problem.name, 'UNPUSHED')
+
+    def test_untracked(self):
+        output = "?? fff\n"
+        l = list(parse.get_problems(output))
+        ok = l.pop()
+        problem = l.pop(0)
+        self.assertEquals(l, [])
+        self.assertEquals(ok.name, 'OK')
+        self.assertEquals(problem.name, 'UNTRACKED')
+
+    def test_uncommitted(self):
+        output = " M src/howsit/test/test_parse.py\n"
+        l = list(parse.get_problems(output))
+        ok = l.pop()
+        problem = l.pop(0)
+        self.assertEquals(l, [])
+        self.assertEquals(ok.name, 'OK')
+        self.assertEquals(problem.name, 'UNCOMMITTED')
+
+
